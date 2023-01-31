8742f8d33ec08e5a18cc14366da052404ba545ab
TryingPop <qotjdgns2001@gmail.com>
2023-01-31 17:42:13 +0900

    fix(Env) : 2023. 1 RoboRobo 강물 독 데미지 같은 환경 데미지 초당 1번씩 적용되게 수정

--- 0_Scripts/Project/RoboRobo/1_Custom/After/3_Environment/TriggerDamage.cs
+++ 0_Scripts/Project/RoboRobo/1_Custom/After/3_Environment/TriggerDamage.cs
@@ -6,10 +6,24 @@ public class TriggerDamage : MonoBehaviour
 {
 
     [SerializeField] private int dmg;
+    private static List<GameObject> objs = new List<GameObject>();
 
-    private void OnTriggerEnter(Collider other)
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!objs.Contains(other.gameObject))
+        {
+
+            objs.Add(other.gameObject);
+            other.gameObject.GetComponent<Stats>().OnDamaged(dmg);
+            StartCoroutine(Timer(other.gameObject));
+        }
+    }
+
+    private IEnumerator Timer(GameObject obj)
     {
 
-        other.gameObject.GetComponent<Stats>().OnDamaged(dmg);
+        yield return new WaitForSeconds(1f);
+        objs.Remove(obj);
     }
 }
