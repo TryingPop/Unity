a3eaad3b6202e96ac81cc1498414ea317ec424d8
TryingPop <qotjdgns2001@gmail.com>
2023-05-13 18:07:09 +0900

    fix(cam) : 2023. 5 Study 카메라 가장자리 수정 교재 382p 카메라 이동 한계선 부분 수정 기존에는 캐릭터의 위치로 화면 이동을 멈추는 형식이라 경계 밖에 캐릭터가 있으면 화면이 이동안하는 코드이므로 의도와 맞이 않다고 생각된다 그래서 카메라 가장자리 값을 확인해 넘어갔는지 판별하고 카메라 위치 수정 단, 경계의 크기가 화면보다 작은 경우 왼쪽과 하단부로 맞추게 설정했다

--- 0_Scripts/Study/2D_Game/02_NinjaSlasherX/01_Player/CameraFollow.cs
+++ 0_Scripts/Study/2D_Game/02_NinjaSlasherX/01_Player/CameraFollow.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum CAMERATARGET    // 카메라에 대한 대상 유형
@@ -70,7 +71,7 @@ public class CameraFollow : MonoBehaviour
     void LateUpdate()
     {
 
-        float targetX = playerTrfm.position.x;
+        float targetX = playerTrfm.position.x;          
         float targetY = playerTrfm.position.y;
         float pX = transform.position.x;
         float pY = transform.position.y;
@@ -82,36 +83,41 @@ public class CameraFollow : MonoBehaviour
         {
 
             case CAMERATARGET.PLAYER:
-                targetX = playerTrfm.position.x;
-                targetY = playerTrfm.position.y;
+                // targetX = playerTrfm.position.x;
+                // targetY = playerTrfm.position.y;
                 break;
-            case CAMERATARGET.PLAYER_MARGIN:
+            case CAMERATARGET.PLAYER_MARGIN:    // 보는 방향으로 가장자리 값만큼 x, y를 더한다
                 targetX = playerTrfm.position.x +
                     param.margin.x * playerCtrl.dir;
                 targetY = playerTrfm.position.y + param.margin.y;
                 break;
-            case CAMERATARGET.PLAYER_GROUND:
+            case CAMERATARGET.PLAYER_GROUND:    // 
                 targetX = playerTrfm.position.x +
                     param.margin.x * playerCtrl.dir;
                 targetY = playerCtrl.groundY + param.margin.y;
                 break;
         }
 
+        /*
+        // 의도하지 않은 코드라 판별해 수정
         // 카메라 이동 한계 경계선 검사
-        if (param.borderCheck)
+        if (param.borderCheck)  // 현재 코드가 많이 이상하다
         {
 
+            // 플레이어가 경계선 밖을 벗어났는지 확인
             float cX = playerTrfm.transform.position.x;
             float cY = playerTrfm.transform.position.y;
-            if (cX < param.borderLeftTop.transform.position.x ||
-                cX > param.borderRightBottom.transform.position.x ||
-                cY > param.borderLeftTop.transform.position.y ||
-                cY < param.borderRightBottom.transform.position.y)
+
+            if (cX < param.borderLeftTop?.transform.position.x ||
+                cX > param.borderRightBottom?.transform.position.x ||
+                cY > param.borderLeftTop?.transform.position.y ||
+                cY < param.borderRightBottom?.transform.position.y)
             {
 
                 return;
             }
-        }
+        }        
+        */
 
         // 플레이어가 카메라 프레임 안에 들어왔는지 검사
         if (param.viewAreaCheck)
@@ -121,9 +127,10 @@ public class CameraFollow : MonoBehaviour
             Vector3 minMargin = param.viewAreaMinMargin;
             Vector3 maxMargin = param.viewAreaMaxMargin;
             Vector2 min = Camera.main.ViewportToWorldPoint(
-                new Vector3(0.0f, 0.0f, z)) - maxMargin;
+                new Vector3(0.0f, 0.0f, z)) - minMargin;
             Vector2 max = Camera.main.ViewportToWorldPoint(
                 new Vector3(1.0f, 1.0f, z)) - maxMargin;
+
             if (playerTrfm.position.x < min.x || playerTrfm.position.x > max.x)
             {
 
@@ -134,6 +141,40 @@ public class CameraFollow : MonoBehaviour
 
                 targetY = playerTrfm.position.y;
                 playerCtrl.groundY = playerTrfm.position.y;
+            }
+        }
+
+        // 카메라 이동 한계 경계선 검사
+        // 의도치 않은 화면을 보일 수 있기에
+        // 경계선 검사를 후자로 미뤄서 우선 순위를 높였다
+        if (param.borderCheck) 
+        {
+
+            float halfHeight = camera.orthographicSize;                     // 화면의 세로 사이즈 절반
+            float halfWidth = camera.orthographicSize * camera.aspect;      // 화면의 가로 사이즈 절반
+
+            // 가로 검사이고 왼쪽 우선
+            if (targetX - halfWidth < param.borderLeftTop.transform.position.x)
+            {
+
+                targetX = param.borderLeftTop.transform.position.x + halfWidth;
+            }
+            else if (targetX + halfWidth > param.borderRightBottom.transform.position.x)
+            {
+
+                targetX = param.borderRightBottom.transform.position.x - halfWidth;
+            }
+
+            // 세로 검사 아래쪽 우선
+            if (targetY - halfHeight < param.borderRightBottom.transform.position.y)
+            {
+
+                targetY = param.borderRightBottom.transform.position.y + halfHeight;
+            }
+            else if (targetY + halfHeight > param.borderLeftTop.transform.position.y)
+            {
+
+                targetY = param.borderLeftTop.transform.position.y - halfHeight;
             }
         }
 
