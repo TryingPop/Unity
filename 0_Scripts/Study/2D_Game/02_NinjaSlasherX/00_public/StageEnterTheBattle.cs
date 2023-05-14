ed267e5fdb9ebcb9ced7348980a681ad5e8fbc0b
TryingPop <qotjdgns2001@gmail.com>
2023-05-14 21:12:08 +0900

    feat(stage) : 2023. 5 Study 게임성을 높이기 위해 스크립트 추가 StageEnterTheBattle은 전투 돌입 시 뒤로 도망을 못가게 하기 위해 콜라이더 충돌을 하면 특정 오브젝트 활성화 기능을 한다 StageHidden은 동굴 중에 히든 장소를 만들어서 들어갈 경우 스프라이트의 알파 값을 낮추어 관통하는 듯한 효과를 보여준다 카메라에서 Culling Mask 를 수정하는 방법도 있지만 여기서는 알파 값으로 수정했다

---
+++ 0_Scripts/Study/2D_Game/02_NinjaSlasherX/00_public/StageEnterTheBattle.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageEnterTheBattle : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] activeObjs;   // 배틀 진입 시 활성화 시킬 오브젝트들
+    [SerializeField] bool once;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        // 플레이어한테만 반응
+        if (collision.tag == "PlayerBody")
+        {
+
+            for (int i = 0; i < activeObjs.Length; i++)
+            {
+
+                activeObjs[i].SetActive(true);
+            }
+
+            if (once)
+            {
+
+                Destroy(gameObject, 1f);
+            }
+        }
+    }
+}
---
+++ 0_Scripts/Study/2D_Game/02_NinjaSlasherX/00_public/StageHiddenSite.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageHiddenSite : MonoBehaviour
+{
+
+    [SerializeField] SpriteRenderer[] sprites;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "PlayerBody")
+        {
+
+            for (int i = 0; i < sprites.Length; i++) 
+            {
+
+                sprites[i].color = new Color(0f, 0f, 0f, 0.8f);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        if (collision.tag == "PlayerBody")
+        {
+
+            for (int i = 0; i < sprites.Length; i++)
+            {
+
+                sprites[i].color = new Color(0f, 0f, 0f, 0.8f);
+            }
+        }
+    }
+}
