3a32603b3b158548c432fa9ec1fa326593ef648e
TryingPop <qotjdgns2001@gmail.com>
2023-05-28 18:10:25 +0900

    fix(pad) : 2023. 5 Study 패드와 플레이어 움직임 연동 추가

--- 0_Scripts/Study/2D_Game/02_NinjaSlasherX/01_Player/PlayerMain.cs
+++ 0_Scripts/Study/2D_Game/02_NinjaSlasherX/01_Player/PlayerMain.cs
@@ -7,6 +7,8 @@ public class PlayerMain : MonoBehaviour
 
     // 캐시
     PlayerController playerCtrl;
+    zFoxVirtualPad vpad;
+    
     bool actionEtcRun = true;
 
     // 코드 (MonoBehaviour 기본 기능 구현)
@@ -14,12 +16,13 @@ public class PlayerMain : MonoBehaviour
     {
 
         playerCtrl = GetComponent<PlayerController>();
+        vpad = GameObject.FindObjectOfType<zFoxVirtualPad>();
     }
 
 
     void Update()
     {
-        
+
         // 조작이 가능한가?
         if (!playerCtrl.activeSts)
         {
@@ -27,12 +30,31 @@ public class PlayerMain : MonoBehaviour
             return;
         }
 
-        // 패드 처리
+        // 가상패드
+        float vpad_vertical = 0.0f;
+        float vpad_horizontal = 0.0f;
+        zFOXVPAD_BUTTON vpad_btnA = zFOXVPAD_BUTTON.NON;
+        zFOXVPAD_BUTTON vpad_btnB = zFOXVPAD_BUTTON.NON;
+        if (vpad != null)
+        {
+
+            vpad_vertical = vpad.vertical;
+            vpad_horizontal = vpad.horizontal;
+            vpad_btnA = vpad.buttonA;
+            vpad_btnB = vpad.buttonB;
+        }
+
+        // 이동
         float joyMv = Input.GetAxis("Horizontal");  // 좌우 키 입력 여부 확인
-        playerCtrl.ActionMove(joyMv);               // 이동 변수 세팅 및 애니메이션 설정
+        joyMv = Mathf.Pow(Mathf.Abs(joyMv), 3.0f) * Mathf.Sign(joyMv);  // joyMv의 절대값에 3제곱을 한 뒤 부호를 맞춰준다
+
+        float vpadMv = vpad_horizontal;
+        vpadMv = Mathf.Pow(Mathf.Abs(vpadMv), 1.5f) * Mathf.Sign(vpadMv);   // vpadMv의 절대값에 1.5제곱을 한 값 부호는 vpad와 동일하게
+        
+        playerCtrl.ActionMove(joyMv + vpadMv);               // 이동 변수 세팅 및 애니메이션 설정
 
         // 점프
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") || vpad_btnA = zFOXVPAD_BUTTON.DOWN)
         {
 
             playerCtrl.ActionJump();                // 점프 실행
@@ -41,10 +63,10 @@ public class PlayerMain : MonoBehaviour
 
         // 공격
         if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2") 
-            || Input.GetButtonDown("Fire3"))
+            || Input.GetButtonDown("Fire3") || vpad_btnB == zFOXVPAD_BUTTON.DOWN)
         {
 
-            if (Input.GetAxisRaw("Vertical") < 0.5f)
+            if (Input.GetAxisRaw("Vertical") + vpad_vertical < 0.5f)
             {
 
                 playerCtrl.ActionAttack();
@@ -58,7 +80,7 @@ public class PlayerMain : MonoBehaviour
         }
 
         // 문을 열거나 통로에 들어간다
-        if (Input.GetAxisRaw("Vertical") > 0.7f)
+        if (Input.GetAxisRaw("Vertical") + vpad_vertical > 0.7f)
         {
             if (actionEtcRun)
             {
