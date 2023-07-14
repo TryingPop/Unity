9283584a5e47964eecd4f772bf25527d58e20142
TryingPop <qotjdgns2001@gmail.com>
2023-07-14 18:54:57 +0900

    feat(db) : 2023. 7 Study 데이터 베이스 매니저 스크립트
    보통은 xml, 엑셀 같은 파일등을 이용하면 알아보기 쉽고 관리하기도
    편하지만
    사용할 데이터가 적어서 스크립트만 이용해서 구현

---
+++ 0_Scripts/Study/2D_Game/03_쯔꾸르/DatabaseManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour
+{
+
+
+    public static DatabaseManager instance;
+
+    private void Awake()
+    {
+        
+        if (instance == null)
+        {
+
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+
+            Destroy(gameObject);
+        }
+    }
+
+    // 1. 씬 이동 A < - > B. 객체 파괴되어 내용 초기화 안되게 관리
+    // 2. 세이브와 로드
+    // 3. 미리 만들어 두면 편하다! 아이템
+    // 원래는 xml같은 것을 이용해야 하는데 양이 적어 스크립트로 관리!
+
+    public string[] var_name;
+    public float[] var;
+
+    public string[] switch_name;
+    public bool[] switches;
+
+
+}
