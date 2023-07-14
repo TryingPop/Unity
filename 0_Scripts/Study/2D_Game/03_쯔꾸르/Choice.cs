8e1c67058a0d77263a5ccc355673ecc4e2ba2c24
TryingPop <qotjdgns2001@gmail.com>
2023-07-14 20:42:50 +0900

    feat(choice) : 2023. 7 Study 선택지 스크립트
    이동방지는 해당 선택지를 사용하는 스크립트에서 만들어야 한다

---
+++ 0_Scripts/Study/2D_Game/03_쯔꾸르/Choice.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Choice
+{
+
+    public string question;     // 질문
+    public string[] answers;    // 답변 (1 ~ 4 개 허용)
+}
