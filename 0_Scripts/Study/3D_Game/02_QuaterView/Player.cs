82399bcfe41dd1019e7ddb8fe1da3c31018daaaf
TryingPop <qotjdgns2001@gmail.com>
2023-07-23 00:08:51 +0900

    fix(move) : 2023. 7 Study 골드메탈 쿼터뷰 영상 보고 만든 플레이어 이동
    멈춘 상태로 스페이스바 누르면 점프이고
    이동 중 스페이스바 누르면 회피 모션
    
    그리고 상태는 하나씩 하게 한다
    점프 파워가 강한데 따라서 ProjectSettinsg에서 Physics 의 중력값을
    조절했따
    
    또한, 벽과 마찰력을 없애기 위해 벽 콜라이더에 제로 값을 담은 물리 마테리얼을 넣어줬따

--- 0_Scripts/Study/3D_Game/02_QuaterView/Player.cs
+++ 0_Scripts/Study/3D_Game/02_QuaterView/Player.cs
@@ -6,23 +6,41 @@ public class Player : MonoBehaviour
 {
 
     public float speed = 20f;
+    public float jumpPower = 15;
 
-    private float hAxis;
-    private float vAxis;
+    private float hAxis;    // 캐릭터 이동 좌우 방향 키
+    private float vAxis;    // 캐릭터 이동 상하 방향 키
 
-    private bool wDown;
+    private bool wDown;     // 캐릭터 걷기 좌측 쉬프트 키 입력
+    private bool jDown;     // 캐릭터 점프 스페이스바 키
 
-    private Vector3 moveVec;
+    private bool isJump;    // 현재 점프 상태 체크
+    private bool isDodge;   // 현재 회피 상태 체크
 
+    private Vector3 moveVec;    // 이동용 벡터3
+    private Vector3 dodgeVec;   // 닷지용 벡터3
     private Animator anim;
+    public Rigidbody rigid;
+
 
     private void Awake()
     {
 
         anim = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
+    {
+
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+        Dodge();
+    }
+
+    private void GetInput()
     {
 
         // GetAxis는 부드럽게 받아오는 반면,   -1f ~ 1f
@@ -32,13 +50,28 @@ public class Player : MonoBehaviour
 
         // ProjectSettings의 InputManager에서 Walk를 추가해줘야한다
         wDown = Input.GetButton("Walk");        // 여기서는 left shift
+        jDown = Input.GetButtonDown("Jump");    // 여기서는 스페이스 바
+
+    }
+
+    private void Move()
+    {
 
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;      // 크기 1로 맞춘다
 
+        if (isDodge)
+        {
+
+            moveVec = dodgeVec;
+        }
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
+    }
+
+    private void Turn()
+    {
 
         // 해당 좌표로 바라본다
         // 그래서 현재 위치 + moveVec을 해준다
@@ -47,5 +80,57 @@ public class Player : MonoBehaviour
         // Vector3 destination = transform.position + moveVec
         // LookAt은 이동 전으로 가야한다
         transform.LookAt(transform.position + moveVec);
+    }
+
+    private void Jump()
+    {
+
+        if (jDown && !isJump && moveVec == Vector3.zero && !isDodge)
+        {
+
+            // 순간적인 힘을 주는 모드인 Impulse
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    private void Dodge()
+    {
+
+        if (jDown && !isJump && moveVec != Vector3.zero && !isDodge)
+        {
+
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            // 해당 이름의 메소드를 0.4초 뒤에 실행
+            Invoke("DodgeOut", 0.4f);
+        }
+    }
+
+    private void DodgeOut()
+    {
+
+        isDodge = false;
+        speed *= 0.5f;
+    }
+
+    // 착지 판정
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        // 태그로 지면 판정
+        // Floor 태그 생성 및 지면에 Floor 태그 추가
+        if (collision.gameObject.tag == "Floor")
+        {
+
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 }
