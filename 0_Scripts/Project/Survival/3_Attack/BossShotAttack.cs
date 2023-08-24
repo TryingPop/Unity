7e1f33bf80195f85fcac61d59907e18d2fa0e6af
TryingPop <qotjdgns2001@gmail.com>
2023-08-24 16:44:19 +0900

    feat(bossSkill) : 2023. 8 Survivor 보스 빅샷 준비

---
+++ 0_Scripts/Project/Survival/3_Attack/BossShotAttack.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShotAttack : Attack
+{
+
+
+    public override void OnAttack(Unit _unit)
+    {
+
+
+    }
+}
