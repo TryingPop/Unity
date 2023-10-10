e1df95c3d8c53b7dde3fff13f44e7fb4b5d4420b
TryingPop <qotjdgns2001@gmail.com>
2023-10-10 23:00:21 +0900

    fix(slot) : 2023. 10 Survivor 슬롯 부분 수정
    기존에는 layoutGroup으로 슬롯 갯수를 했지만
    유닛의 hp 를 표기할 예정이기에 잦은 변화가 예상되어
    layoutgroup 이 아닌 스크립트로 관리하게 했다
    
    할당된 ui 크기에 맞춰 유닛 슬롯을 배치하는 것은 이상없이 된다
    그리고 선택된 유닛이 최대로 보일 수 있는 칸보다 많은 경우
    버튼을 다음 활성화 시키고 다음 버튼을 누르면 해당 유닛 뒤쪽을 보이게
    하려고 한다
    
    지금은 스크립트 작성 중이라 완성되지 않은 스크립트이다

--- 0_Scripts/Project/Survival/02_Manager/SlotManager.cs
+++ 0_Scripts/Project/Survival/02_Manager/SlotManager.cs
@@ -8,23 +8,204 @@ public class SlotManager : MonoBehaviour
 
     [SerializeField] protected GameObject unitSlotObj;
 
-    [SerializeField] protected Transform slotUITrans;
-    protected GameObject[] slots;
+    [SerializeField] protected RectTransform slotUIRectTrans;
 
-    private void Awake()
+    protected List<RectTransform> slots;                            // 크기가 변하므로 리스트로!
+
+    [SerializeField] protected Vector2 spacing;
+
+    [Tooltip("x : left, y : right, z : top, w : bottom")]
+    [SerializeField] protected Vector4 padding;
+
+    protected Vector2 uiSize;
+    protected Vector2 slotSize;
+
+    protected List<Selectable> units;
+
+    [SerializeField] protected GameObject nextBtn;
+    [SerializeField] protected GameObject prevBtn;
+
+    protected byte curPage;
+    protected byte maxPage;
+
+    /// <summary>
+    /// 0 : maxRow, 1 : maxColumn
+    /// </summary>
+    protected int[] matrixSize = new int[2];  
+
+
+
+    private void Start()
     {
 
-        slots = new GameObject[VariableManager.MAX_SELECT];
+        slots = new List<RectTransform>();
+        units = InputManager.instance.curGroup.Get();
 
-        for (int i = 0; i < VariableManager.MAX_SELECT; i++)
+        SetCalcSize();
+        SetMaxSlots();
+        BatchSlot();
+
+        if (curPage < maxPage) NextPage(0);
+    }
+
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
         {
 
-            slots[i] = Instantiate(unitSlotObj, slotUITrans);
-            
-            // slots[i].SetActive(false);
+            SetCalcSize();
+            SetMaxSlots();
+            BatchSlot();
+            if (curPage < maxPage) NextPage(0);
+        }
+    }
+
+    private void SetCalcSize()
+    {
+
+        uiSize = slotUIRectTrans.rect.size;
+        slotSize = unitSlotObj.GetComponent<RectTransform>().sizeDelta;
+
+        // spacing + slotSize로 나누면 들어갈 수 있는 숫자로 깔끔하게 만들기 위해 스케일링!
+        uiSize.x += spacing.x - (padding.x + padding.y);
+        uiSize.y += spacing.y - (padding.z + padding.w);
+
+        int row = Mathf.FloorToInt(uiSize.x / (slotSize.x + spacing.x));
+        int column = Mathf.FloorToInt(uiSize.y / (slotSize.y + spacing.y));
+
+        if (row < 1) row = 1;
+        if (column < 1) column = 1;
+
+        matrixSize[0] = row;
+        matrixSize[1] = column;
+
+        curPage = 0;
+        maxPage = (byte)((units.Count - 1) / (row + column));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void SetMaxSlots()
+    {
+
+        int num = matrixSize[0] * matrixSize[1];
+        if (num > VariableManager.MAX_SELECT) num = VariableManager.MAX_SELECT;
+
+        // 생성과 파괴부분 수정할 필요가 있다!
+        if (num < slots.Count)
+        {
+
+            for (int i = slots.Count - 1; i >= num ; i--)
+            {
+
+                var go = slots[i].gameObject;
+                slots.RemoveAt(i);
+                Destroy(go);
+            }
+        }
+        else
+        {
+
+            for (int i = slots.Count; i < num; i++)
+            {
+
+                var go = Instantiate(unitSlotObj, slotUIRectTrans);
+                slots.Add(go.GetComponent<RectTransform>());
+            }
+        }
+    }
+
+    private void BatchSlot()
+    {
+
+        // 라인에 배치할 숫자 구하기
+        float halfSlotSizeX = slotSize.x * 0.5f;
+        float halfSlotSizeY = slotSize.y * 0.5f;
+
+        float batchX = slotSize.x + spacing.x;
+        float batchY = -(slotSize.y + spacing.y);
+
+        for (int i = 0; i < matrixSize[1]; i++)
+        {
+
+            for (int j = 0; j < matrixSize[0]; j++)
+            {
+
+                float posX = j * (batchX) + padding.x + (halfSlotSizeX);
+                float posY = i * (batchY) - padding.z - (halfSlotSizeY);
+
+                if (j + (i * matrixSize[0]) >= slots.Count) break;
+                slots[j + (i * matrixSize[0])].anchoredPosition = new Vector2(posX, posY);
+            }
+        }
+    }
+
+
+    private void ActiveSlot(int _slotIdx, int _unitIdx, bool _active)
+    {
+
+        slots[_slotIdx].gameObject.SetActive(_active);
+    }
+
+    private void SetBtn(int _num)
+    {
+
+        if (_num >= maxPage)
+        {
+
+            _num = maxPage;
+            nextBtn.SetActive(false);
+        }
+        else
+        {
+
+            nextBtn.SetActive(true);
+        }
+
+
+        if (_num <= 0)
+        {
+
+            _num = 0;
+            prevBtn.SetActive(false);
+        }
+        else
+        {
+
+            prevBtn.SetActive(true);
+        }
+
+        curPage = (byte)_num;
+    }
+
+    public void NextPage(int _step)
+    {
+
+        int page = curPage + _step;
+        SetBtn(page);
+
+        int len = matrixSize[0] * matrixSize[1];
+        for (int i = 0; i < len; i++)
+        {
+
+            if (i >= slots.Count) break;
+            int unitIdx = (len * curPage) + i;
+            if (units.Count > unitIdx)
+            {
+
+                ActiveSlot(i, unitIdx, true);
+            }
+            else
+            {
+
+                ActiveSlot(i, unitIdx, false);
+            }
         }
     }
 }
+
 
 
 // 해당 사이트 참고해서 스크롤 뷰 풀링 하는거 고려해보기!
