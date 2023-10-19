d43dda0e837135eb0c43a313b287e2ebe79de0bc
TryingPop <qotjdgns2001@gmail.com>
2023-10-19 14:24:25 +0900

    fix(menuUI) : 2023. 10 Survivor 화면 해상도 조절 스크립트
    https://www.youtube.com/watch?v=_LK1YQ2ff2k
    참고했다
    
    드랍다운 초기화에 사용되는 로직은 다음과 같다
    먼저 화면 해상도 값을 리스트에 전부 받아온다
    Screen.resolutions 를 이용한다
    선택된 인덱스로 변환할 것이기에 인덱스 변수 selectNum을 추가해준다
    
    다음으로 이제 Dropdown의 선택 목록에 해당 값들을 입력한다
    먼저 드랍다운 목록을 초기화 해줘야한다
    현재 해상도 값의 인덱스를 selectNum에 넣어준다
    
    여기까지가 드랍다운 초기화에 사용되는 메서드이다
    이후엔 드랍다운에 값을 selectNum에 연동하는 메서드를 만든다
    
    그리고 확인용 버튼을 만들어 버튼을 누르면 해상도를 변경하게 해주면
    끝이다
    해상도를 바꾸는 방법은 Screen.SetResoultion을 이용한다

--- 0_Scripts/Project/Survivor/10_Scene/MenuOption.cs
+++ 0_Scripts/Project/Survivor/10_Scene/MenuOption.cs
@@ -1,21 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 소리 조절 기능 추가할꺼
 /// </summary>
 public class MenuOption : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private List<Resolution> resolutions = new List<Resolution>();
+    public Dropdown resolutionDropdown;         // 연동시킬 드랍다운
+    private Toggle fullScreenBtn;               // 풀스크린 확인용 토글 키
+
+    private int selectNum;                      // 선택된 드랍다운 값
+    private FullScreenMode screenMode;          // 
+
+    private void Start()
     {
-        
+
+        InitUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    // 시작 시 화면 정보 가져온다
+    private void InitUI()
     {
-        
+
+        // resolutions.AddRange(Screen.resolutions);    // 화면 정보를 다 받아온다
+
+        // 60hz만 받아온다
+        for (int i = 0; i < Screen.resolutions.Length; i++)
+        {
+
+            if (Screen.resolutions[i].refreshRate == 60) resolutions.Add(Screen.resolutions[i]);
+        }
+
+        resolutionDropdown.options.Clear();
+
+        int optionNum = 0;
+        for (int i = 0; i < resolutions.Count; i++)
+        {
+
+            // 설정 가능한 화면 정보들 다 나온다
+            // Debug.Log($"{resolutions[i].width} X {resolutions[i].height} {resolutions[i].refreshRate}");
+            
+            Dropdown.OptionData option = new Dropdown.OptionData();
+            option.text = $"{resolutions[i].width} X {resolutions[i].height} {resolutions[i].refreshRate}hz";
+            resolutionDropdown.options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+
+                resolutionDropdown.value = optionNum;
+            }
+            optionNum++;
+        }
+
+        // 새로 고침
+        resolutionDropdown.RefreshShownValue();
+        fullScreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow) ? true : false;
+
+    }
+
+    // 드랍다운 이벤트 체인지에 연결
+    private void DropboxOptionChange(int num)
+    {
+
+        selectNum = num;
+    }
+
+    /// <summary>
+    /// 저장 버튼과 연동
+    /// </summary>
+    public void OkBNtnClick()
+    {
+
+        Screen.SetResolution(resolutions[selectNum].width,
+            resolutions[selectNum].height,
+            screenMode);
+    }
+
+    public void FullScreenBtn(bool isFull)
+    {
+
+        screenMode = isFull ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
     }
 }
