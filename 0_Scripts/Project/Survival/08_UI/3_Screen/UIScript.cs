d4eb20f0cb6a89516fe16cd000b7f3a051312b12
TryingPop <qotjdgns2001@gmail.com>
2023-11-03 17:45:39 +0900

    feat(ui) : 2023. 11 Survivor 대사 ui 추가
    튜토리얼을
    퀘스트 > 설명용 대사 > 대사 ... > 퀘스트 완료 > 대사 > ... > 시나리오
    종료
    순으로 진행할 예정

--- 0_Scripts/Project/Survival/08_UI/3_Screen/UIScript.cs
+++ 0_Scripts/Project/Survival/08_UI/3_Screen/UIScript.cs
@@ -7,10 +7,49 @@ public class UIScript : MonoBehaviour
 {
 
     [SerializeField] private Text scriptTxt;
+    [SerializeField] private RectTransform myRectTrans;
+    [SerializeField] private Vector3 initPos;
+    [SerializeField] private Vector3 destPos;
+    private Vector3 destination;
+    private float startTime;
+    private float endTime;
 
-    public void Init(string _text)
+    public void Init(string _text, ref Vector3 _destination, float _time = 5.0f)
+    {
+
+        scriptTxt.text = _text;
+        destination = _destination;
+        startTime = Time.time;
+        endTime = _time;
+    }
+
+    // 위치 조절
+    public void SetPos()
+    {
+
+        if (Time.time - startTime < endTime)
+        {
+
+            myRectTrans.anchoredPosition = Vector3.Lerp(myRectTrans.anchoredPosition, destination, 0.1f);
+        }
+        else
+        {
+
+            myRectTrans.anchoredPosition = initPos;
+        }
+    }
+
+    // Test 용도
+    public void Update()
     {
         
-        scriptTxt.text = _text;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            
+            Init("내용 글", ref destPos);
+        }
+
+        SetPos();
     }
 }
